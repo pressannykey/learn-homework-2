e7cdd15f1f6828012c0df344b3145b8c8027817c
Angelina Groza <angelina.groza@mail.ru>
2020-02-24 01:12:09 +0300

    ephem_bot task completed

--- ephem_bot.py
+++ ephem_bot.py
@@ -13,22 +13,15 @@
 
 """
 import logging
+import ephem
+import settings
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
-)
-
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn', 
-        'password': 'python'
-    }
-}
+                    )
 
 
 def greet_user(bot, update):
@@ -38,21 +31,42 @@ def greet_user(bot, update):
 
 
 def talk_to_me(bot, update):
-    user_text = update.message.text 
+    user_text = update.message.text
     print(user_text)
     update.message.reply_text(user_text)
- 
+
+
+def get_constellation(bot, update):
+    input = update.message.text.split()
+    if len(input) > 1:
+        planet = input[1].capitalize()
+        today = update.message.date
+        try:
+            result = ephem.constellation(getattr(ephem, planet)(today))
+            text = 'Cегодня {} в созвездии {}!'.format(planet, result[1])
+            update.message.reply_text(text)
+        except AttributeError:
+            text = 'Такой планеты не существует'
+            update.message.reply_text(text)
+    else:
+        text = 'Некорректный ввод'
+        update.message.reply_text(text)
+
+    logging.info('User: %s, Input: %s --> Output: %s',
+                 update.message.chat.username, update.message.text, text)
+
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
-    
+    mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
+
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    
+    dp.add_handler(CommandHandler("planet", get_constellation))
+
     mybot.start_polling()
     mybot.idle()
-       
+
 
 if __name__ == "__main__":
     main()
